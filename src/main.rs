36ba94ce4787643c1b9038386cbd0f884094065d
Grisha Bordyugov <gregbg@gmail.com>
2021-03-27 10:59:23 +0100

    add enum State

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+#[derive(Debug)]
+enum State {
+    Match,
+    Char { c: char, next: Box<State> },
+    Split { first: Box<State>, second: Box<State> },
+}
+
 fn main() {
     println!("Hello, world!");
 }
