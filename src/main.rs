963a2c0a16a9850060854fbd2265b82f8cdf8c61
Grigory Bordyugov <gregbg@gmail.com>
2021-03-30 14:47:24 +0200

    one error away from the truth

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,17 @@ use std::rc::Rc;
 #[derive(Debug)]
 enum State {
     Match,
-    Char { c: char, next: Rc<State> },
-    // Split { first: Rc<State>, second: Rc<State> },
+    Char { c: char, next: StateP },
 }
 
 type StateP = Rc<State>;
 
-fn print_state<'a>(s: State) {
-    match s {
+fn print_state(s: StateP) {
+    match *s {
         State::Match => print!("Match"),
         State::Char { c, next } => {
             print!("{}", c);
-            print_state(Rc::as_ptr(&next));
+            print_state(*next);
         },
         // State::Split { first, second } => {
         //     print_state(*first);
@@ -31,7 +30,7 @@ fn main() {
     // let s = State::Split { first: Rc::clone(&mp), second: Rc::clone(&cp) };
     // let sp = Rc::new(s);
 
-    print_state(*mp);
-    print_state(*cp);
+    print_state(mp);
+    print_state(cp);
     // print_state(*sp);
 }
