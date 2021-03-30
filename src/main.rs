3845b70a6f825c63800b5f17d736d77ddacf80fc
Grigory Bordyugov <gregbg@gmail.com>
2021-03-30 14:25:51 +0200

    nothing is really working

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,37 @@
+use std::rc::Rc;
+
 #[derive(Debug)]
-enum State<'a, 'b, 'c> {
+enum State {
     Match,
-    Char { c: char, next: &'a State<'a, 'b, 'c> },
-    Split { first: &'b State<'a, 'b, 'c>, second: &'c State<'a, 'b, 'c> },
+    Char { c: char, next: Rc<State> },
+    // Split { first: Rc<State>, second: Rc<State> },
 }
 
-fn print_state<'a>(s: &'a State) {
+type StateP = Rc<State>;
+
+fn print_state<'a>(s: State) {
     match s {
         State::Match => print!("Match"),
         State::Char { c, next } => {
             print!("{}", c);
-            print_state(&next);
+            print_state(Rc::as_ptr(&next));
         },
-        State::Split { first, second } => {
-            print_state(&first);
-            print_state(&second);
-        },
+        // State::Split { first, second } => {
+        //     print_state(*first);
+        //     print_state(*second);
+        // },
     }
 }
 
 fn main() {
     let m = State::Match;
-    let c = State::Char { c: 'c', next: &m };
-    let s = State::Split { first: &m, second: &c} ;
+    let mp = Rc::new(m);
+    let c = State::Char { c: 'c', next: Rc::clone(&mp) };
+    let cp = Rc::new(c);
+    // let s = State::Split { first: Rc::clone(&mp), second: Rc::clone(&cp) };
+    // let sp = Rc::new(s);
 
-    print_state(&m);
-    print_state(&c);
-    print_state(&s);
+    print_state(*mp);
+    print_state(*cp);
+    // print_state(*sp);
 }
